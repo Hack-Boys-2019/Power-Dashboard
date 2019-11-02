1763b3c27e8a71894d1689a8282abeb824ac0ced
Tom Paoloni <paoloni.6@osu.edu>
2019-11-02 15:57:34 -0400

    Added some chartjs stuff

---
+++ powerdash-react/src/Main.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Main.css';
+import Chart from 'react-chartjs-2'
+
+function Main() {
+    var speedData = {
+        labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
+        datasets: [{
+          label: "Car Speed",
+          data: [0, 59, 75, 20, 20, 55, 40],
+        },
+        {
+            label: "Stuff",
+            data: [0, 10, 20, 30, 40, 50, 60],
+            backgroundColor: "rgb(255,0,0,0.3)",
+            borderColor: "rgb(255,0,0)"
+          }]
+      };
+       
+      var chartOptions = {
+        legend: {
+          display: true,
+          position: 'top',
+          labels: {
+            boxWidth: 80,
+            fontColor: 'black'
+          }
+        }
+      };
+    return (
+        <div>
+            <div id="sunburst"></div>
+            <div id="line-graph"></div>
+            <div id="analogy"></div>
+            <div id="map"></div>
+            <Chart
+            type="line"
+            data={speedData}
+            width={100}
+            height={50}
+            options={chartOptions}
+            />
+        </div>
+
+        
+    );
+}
+
+export default Main;
