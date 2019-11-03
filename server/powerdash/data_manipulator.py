40146cc97c951096083c4080f39a862c90de83fe
Greshbolt <greshbolt@gmail.com>
2019-11-03 01:17:52 -0500

    Added testing functionality to data manipulator

--- server/powerdash/data_manipulator.py
+++ server/powerdash/data_manipulator.py
@@ -1,11 +1,17 @@
 from generator import generator
+import pandas as pd
 class DataManipulator:
     def __init__(self):
         pass
-    def avg(self, type, id, start_time, end_time):
+    def avg(self, type, id, start_time, end_time, database):
         pass
-    def total(self, type, id, start_time, end_time):
+    def total(self, type, id, start_time, end_time, database):
         pass
-
+    def test_values(self,id,database):
+        df = pd.read_sql_query(f'SELECT MeterId,CurrentValue FROM `table` WHERE MeterId = \'{id}\' ORDER BY ROWID ;', database)
+        return df
 g = generator()
-g.initialize_generator('HackathonConfig.csv','HackathonDataHourly1of2.csv','HackathonDataHourly2of2.csv','HackathonDataDaily.csv')
+daily_db = g.return_daily_db()
+hourly_db = g.return_hourly_db()
+d = DataManipulator()
+print(d.test_values('OSU_RTS.MDBUS_E3.001_E01',daily_db))
--- server/powerdash/generator.py
+++ server/powerdash/generator.py
@@ -6,7 +6,8 @@ class generator:
     def __init__(self):
         self.meter_dict = {}
         self.building_dict = {}
-        self.csv_database = create_engine('sqlite:///hourly_meter.db')
+        self.hourly_database = create_engine('sqlite:///hourly_meter.db')
+        self.daily_database = create_engine('sqlite:///daily_meter.db')
     def config_generation(self,file_name):
         reader = pd.read_csv(file_name, names=['BldId', 'MeterId', 'Description', 'Units', 'Resource', 'BuildingName','SqrFeet','BuildDate','Lat','Long','Campus','Organization','LocationType','SteamID', 'ChilledWater','HotWaterID','ActivationDate'])
         for i in range(len(reader)):
@@ -17,7 +18,7 @@ class generator:
                 self.building_dict[building_id] = building(building_id,r_rdr[5],r_rdr[6],r_rdr[7],r_rdr[8],r_rdr[9],r_rdr[10],r_rdr[11],r_rdr[12],r_rdr[13],r_rdr[14],r_rdr[15])
             if not meter_id in self.meter_dict:
                 self.meter_dict[meter_id] = meter(meter_id,r_rdr[2],r_rdr[3],r_rdr[4],building_id,r_rdr[16])
-    def data_generation(self,file_name):
+    def data_generation(self,file_name, csv):
         i = 0
         chunksize = 100000
         i = 0
@@ -26,15 +27,16 @@ class generator:
             df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) 
             df.index += j
             i+=1
-            df.to_sql('table', self.csv_database, if_exists='append')
+            df.to_sql('table', csv, if_exists='append')
             j = df.index[-1] + 1
-    def change_table(self, name):
-        self.csv_database = create_engine(name)
     def building_report(self):
         return self.building_dict
     def initialize_generator(self,config, hourly_one, hourly_two, daily):
         self.config_generation(config)
-        self.data_generation(hourly_one)
-        self.data_generation(hourly_two)
-        self.change_table('sqlite:///daily_meter.db')
-        self.data_generation(daily)
+        self.data_generation(hourly_one, self.hourly_database)
+        self.data_generation(hourly_two, self.hourly_database)
+        self.data_generation(daily, self.daily_database)
+    def return_hourly_db(self):
+        return self.hourly_database
+    def return_daily_db(self):
+        return self.daily_database
